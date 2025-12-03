87c8397c4d18dcf8431e546c77ce1aa6ad4a62df
Guillermo Martínez <guillemtnzmtnz@gmail.com>
2025-12-03 11:11:54 +0100

    Create Combate.java
    Boceto clase Combate

---
+++ PROYECTO_POKEMON/src/proyecto/Combate.java
@@ -0,0 +1,26 @@
+package proyecto;
+
+public class Combate {
+	
+	private Pokemon miPokemon;
+	private Pokemon pokemonRival;
+	
+	private Movimiento movimiento;
+	
+	public void atacar(Pokemon yo, Pokemon rival, Movimiento ataque){
+		
+	};
+	
+	public void usarObjeto() {
+		
+	};
+	
+	public void cambiarPokemon(){
+		
+	}
+	
+	public void huir() {
+		
+	}
+
+}
