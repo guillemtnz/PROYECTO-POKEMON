c842cc7669b3bc7d70785fdb81003715cd57073b
Guillermo Martínez <guillemtnzmtnz@gmail.com>
2026-01-08 10:01:27 +0100

    Creacion de clase Pokemon
    He creado la clase con los atributos principales para cada pokemon.
    
    Me faltan la colección de movimientos, los tipos y estados.

---
+++ PROYECTO_POKEMON/src/proyecto/Pokemon.java
@@ -0,0 +1,28 @@
+package proyecto;
+
+public class Pokemon {
+	
+	private String nombre;
+	private String mote;
+	private int vilatidad;
+	private int ataque;
+	private int defensa;
+	private int ataqueEspecial;
+	private int defensaEspecial;
+	private int velocidad;
+	private int estamina;
+	private int nivel;
+	
+	//COLECCION DE MOVIMIENTOS
+	
+	private int fertilidad;
+	private String sexo;
+	
+	//COLECCION DE TIPOS
+	
+	//ESTADOS
+	
+	
+	
+
+}
