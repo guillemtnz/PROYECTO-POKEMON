a0de70d5cdfa0527b123a97c98f3f20f4fbbc4b7
JulioCesarCV <juliocachacavaldez@gmail.com>
2026-01-20 12:00:27 +0100

    Update Tipo.java

--- PROYECTO_POKEMON/src/proyecto/Tipo.java
+++ PROYECTO_POKEMON/src/proyecto/Tipo.java
@@ -1,8 +1,18 @@
 package proyecto;
 
 public enum Tipo {
-	AGUA, BICHO, DRAGON, ELECTRICO, FANTASMA, FUEGO, HIELO, LUCHA,
-	NORMAL, PLANTA, PSIQUICO, ROCA, TIERRA, VENENO, VOLADOR, HADA
+	ACERO, AGUA, BICHO, DRAGON, ELECTRICO, FANTASMA, FUEGO, HADA,
+	HIELO, LUCHA, NORMAL, PLANTA, PSIQUICO, ROCA, SINIESTRO, 
+	TIERRA, VENENO, VOLADOR;
 	
-
+	//Tabla de efectividades ATACANTE X DEFENSOR (en el orden que se ha enumerado) 
+	private static final double[][] EFECTIVIDAD = {
+			
+			
+	};
+	
+	
+	
+	
 }
+
