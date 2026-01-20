42190e4066a79065e2b638429b2b5035080cdc9a
JulioCesarCV <juliocachacavaldez@gmail.com>
2026-01-20 17:44:12 +0100

    Avance clase Tipo
    Creada la matriz de efectividades y empezada clase interna ataque

--- PROYECTO_POKEMON/src/proyecto/Tipo.java
+++ PROYECTO_POKEMON/src/proyecto/Tipo.java
@@ -6,13 +6,59 @@ public enum Tipo {
 	TIERRA, VENENO, VOLADOR;
 	
 	//Tabla de efectividades ATACANTE X DEFENSOR (en el orden que se ha enumerado) 
+	//Static porque es la misma para TODOS los pokemon y Final porque no se modifica
 	private static final double[][] EFECTIVIDAD = {
-			
-			
+		//En el mismo orden que el enumerado	
+		// ACE  AGU  BIC  DRA  ELE  FAN  FUE  HAD  HIE  LUC  NOR  PLA  PSI  ROC  SIN  TIE  VEN  VOL
+	      {0.5,0.5,1,   1,  0.5,1,  0.5,1,  1.5,1,  1,  1,  1,  1.5,1,  1,  1,  1}, // ACERO
+	      {1,  0.5,1,  0.5,1,  1,  1.5,1,  1,  1,  1,  0.5,1,  1.5,1,  1.5,1,  1}, // AGUA
+	      {0.5,1,  1,  1,  1,  0.5,0.5,0.5,1,  0.5,1,  1.5,1.5,1,  1.5,1,  0.5,0.5}, // BICHO
+	      {0.5,1,  1,  1.5,1,  1,  1,  0,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1}, // DRAGON
+	      {1,  1.5,1,  0.5,0.5,1,  1,  1,  1,  1,  1,  0.5,1,  1,  1,  0,  1,  1.5}, // ELECTRICO
+	      {1,  1,  1,  1,  1,  1.5,1,  1,  1,  1,  0,  1,  1.5,1,  0.5,1,  1,  1}, // FANTASMA
+	      {1.5,0.5,1.5,0.5,1,  1,  0.5,1,  1.5,1,  1,  1.5,1,  0.5,1,  1,  1,  1}, // FUEGO
+	      {0.5,1,  1,  1.5,1,  1,  0.5,1,  1,  0.5,1,  1,  1,  1,  1.5,1,  0.5,1}, // HADA
+	      {0.5,0.5,1,  1.5,1,  1,  0.5,1,  0.5,1.5,1,  1.5,1,  1,  1,  1.5,1,  1.5}, // HIELO
+	      {1.5,1,  0.5,1,  1,  0,  1,  0.5,1.5,1,  1.5,1,  0.5,1.5,1.5,1,  0.5,0.5}, // LUCHA
+	      {0.5,1,  1,  1,  1,  0,  1,  1,  1,  1,  1,  1,  1,  0.5,1,  1,  1,  1}, // NORMAL
+	      {0.5,1.5,0.5,0.5,1,  1,  0.5,1,  1,  1,  1,  0.5,1,  1.5,1,  1.5,0.5,0.5}, // PLANTA
+          {0.5,1,  1,  1,  1,  1,  1,  1,  1,  1.5,1,  1,  0.5,1,  0,  1,  1.5,1}, // PSIQUICO
+	      {0.5,1,  1.5,1,  1,  1,  1.5,1,  1.5,0.5,1,  1,  1,  1,  1,  0.5,1,  1.5}, // ROCA
+	      {1,  1,  1,  1,  1,  1.5,1,  0.5,1,  0.5,1,  1,  1.5,1,  0.5,1,  1,  1}, // SINIESTRO
+	      {1,  1,  0.5,1,  1.5,1,  1.5,1,  1,  1,  1,  0.5,1,  1.5,1,  1,  1.5,0}, // TIERRA
+	      {0,  1,  1,  1,  1,  0.5,1,  1.5,1,  1,  1,  1.5,1,  0.5,1,  0.5,0.5,1}, // VENENO
+	      {0.5,1,  1.5,1,  0.5,1,  1,  1,  1,  1.5,1,  1.5,1,  0.5,1,  1,  1,  1}  // VOLADOR			
 	};
 	
+	public double efectividadContra(Tipo defensor) {
+	    return EFECTIVIDAD[this.ordinal()][defensor.ordinal()];
+	}
 	
-	
-	
+		//Clase interna ataque
+		public static class Ataque {
+			
+			//Atributos privados(te miro y te encapsulo)
+			private String nombre;
+			private Tipo tipo;
+			private int potencia;
+			
+			public Ataque(String nombre, Tipo tipo, int potencia) {
+				this.nombre = nombre;
+				this.tipo = tipo;
+				this.potencia = potencia;
+			}
+			
+			public String getNombre() {
+				return nombre;
+			}
+			
+			public Tipo getTipo() {
+				return tipo;
+			}
+			
+			public int getPotencia() {
+				return potencia;
+			}
+		}
 }
 
